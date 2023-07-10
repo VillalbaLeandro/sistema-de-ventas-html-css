ec41591732caa4946f9635df517b0d3b6c6b42d0
VillalbaLeandro <exemprary@gmail.com>
2023-07-09 23:58:42 -0300

    se agrego una img de la estrucutra de la ER

--- php/insertar_usuario.php
+++ php/insertar_usuario.php
@@ -10,12 +10,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tel = $_POST["telNum"];
     $direccion = $_POST["direccion"];
     $tipoCliente = $_POST["tipo_cliente"];
-<<<<<<< HEAD
 
     // Insertar los datos en la base de datos. se coloca 3 porque va a ser por defecto ese rol de usuario (cliente)
     $query = "INSERT INTO usuario (nombre, apellido, cuit_cuil, tel, direccion, rol_id, tipo_cliente_id) 
               VALUES ('$nombre', '$apellido', '$cuil_cuit', '$tel', '$direccion', 3, $tipoCliente)";
-=======
     $email = $_POST["mail"];
     $pass = $_POST["pass"];
     
@@ -23,22 +21,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Insertar los datos en la base de datos. se coloca 3 porque va a ser por defecto ese rol de usuario (cliente)
     $query = "INSERT INTO usuario (nombre, apellido, cuit_cuil, tel, direccion, rol_id, tipo_cliente_id, email, pass) 
             VALUES ('$nombre', '$apellido', '$cuil_cuit', '$tel', '$direccion', 3, $tipoCliente, '$email' , '$pass')";
->>>>>>> c8323a941639b175d390048752c8a686bcc1cf0a
 
     if ($conexion->query($query) === TRUE) {
         echo "Usuario registrado exitosamente.";
     } else {
-<<<<<<< HEAD
         echo "Error al registrar el usuario: " . $conexion->error;
-=======
         echo "Error al registrar el usuario: ";
->>>>>>> c8323a941639b175d390048752c8a686bcc1cf0a
     }
 
     // Cerrar la conexión a la base de datos
     $conexion->close();
 }
-<<<<<<< HEAD
 ?>
 =======
 >>>>>>> c8323a941639b175d390048752c8a686bcc1cf0a
